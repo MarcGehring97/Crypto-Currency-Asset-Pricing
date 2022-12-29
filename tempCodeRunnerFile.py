43c4641be6096c29f3ecfc9e6dc88874b7a22104
MarcGehring97 <marc_gehring@gmx.de>
2022-12-29 23:33:08 +0100

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,17 @@
 """
-This file can be used to download a csv file of price, market cap, and volume timeseries data for 
-a group of cryptocurrencies that can be filtered by specified criteria using the free CoinGecko 
-(https://www.coingecko.com/) API. The Github repository can be found at https://github.com/man-c/pycoingecko/
-and the documentation of the CoinGecko API can be found at https://www.coingecko.com/en/api/documentation.
-The user should make sure that the needed libraries are installled (pandas and pycoingecko). The user can
-also use his/her CoinGecko Pro API Key. The user may also add further filtering criteria and change the 
-chose time period. Finally, the user should specify the destination path where the file will be stored 
-at the bottom of this file. The progress may sometimes stop due to the limit imposed upon the number of 
-API calls per minute of 50. The last successful starting index is printed and also used at the end of the
-file names.
+This file can be used to download a csv file of price, market cap, and volume timeseries data for a group of 
+cryptocurrencies that can be filtered by specified criteria using the free CoinGecko (https://www.coingecko.com/) 
+API. The Github repository can be found at https://github.com/man-c/pycoingecko/ and the documentation of the 
+CoinGecko API can be found at https://www.coingecko.com/en/api/documentation. The user should make sure that the 
+required libraries are installled (pandas and pycoingecko). 
+The user can also use his/her CoinGecko Pro API Key. The user may also add further filtering criteria and change 
+the chosen time period. For convenience, this code allows the user to download the data set in several data 
+subsets. The user may specify the size of every data subset. Finally, the user should change the variable "path" 
+that determines where the file will be stored. The progress may sometimes stop due to the limit imposed upon the 
+number of API calls per minute of 50. The last successfully used starting index for the while loop in the main 
+function "create_data_files" is printed and also indicated at the end of the file names. If the process is 
+interrupted for any reason the user can call the main function at the bottom of this file with a new starting 
+index 'create_data_files(starting_index = <new starting index>)'.
 """
 
 #! import os
@@ -26,36 +29,35 @@ cg = CoinGeckoAPI()
 # checking whether the API works
 print(cg.ping())
 
-# this function takes in a crude list of coin IDs and returns a filtered list
-def filtered_ids(coin_list, starting_index, number_of_ids_per_data_subset):
+# this function takes in a crude list of coin IDs and returns a filtered list of a predefined size
+def filtered_ids(coin_list, starting_index, ids_per_data_subset):
     index = starting_index
     percentage_counter = 1
     ids = []
     print("The filtering progress for this data subset is: ")
     while True:
-        try:
-            # if the end of the whole list is reached
-            if index + 1 <= len(coin_list):
-                # if the number of IDs per data subset is reached
-                if len(ids) < number_of_ids_per_data_subset:
-                    # filtering out coins with a current market capitalization of less than 1m USD
+        # if the end of the whole list is reached
+        if index + 1 <= len(coin_list):
+            # if the number of IDs per data subset is reached
+            if len(ids) < ids_per_data_subset:
+                # filtering out coins with a current market capitalization of less than 1m USD
+                try:
                     if cg.get_coin_by_id(coin_list[index]["id"])["market_data"]["market_cap"]["usd"] >= 1000000:
                         # this might take a while
                         ids.append(coin_list[index]["id"])
-                        index += 1
-
-                        # printing the progress 
-                        progress = int((index - starting_index) / number_of_ids_per_data_subset * 100)
-                        if progress >= percentage_counter:
-                            percentage_counter += 1
-                            print(str(progress) + "%")
-                else:
-                    break
+                except:
+                    print("There is a missing key for " + coin_list[index]["id"]+ " but the process will continue")
+                index += 1
+                # printing the progress 
+                progress = int((index - starting_index) / ids_per_data_subset * 100)
+                if progress >= percentage_counter:
+                    percentage_counter += 1
+                    print(str(progress) + "%")
             else:
                 break
-        except:
-            print("There is a missing key for " + id["id"]+ " but the process will continue")
-            index += 1
+        else:
+            break
+        
 
     # checking for duplicate IDs (there are usually none)
     for id in ids:
@@ -71,8 +73,8 @@ start_date = time.mktime((2011, 1, 1, 12, 0, 0, 4, 1, -1))
 end_date = time.time()
 
 # checking if the Unix dates are correct
-print(datetime.datetime.fromtimestamp(start_date))
-print(datetime.datetime.fromtimestamp(end_date))
+# print(datetime.datetime.fromtimestamp(start_date))
+# print(datetime.datetime.fromtimestamp(end_date))
 
 # creating a data frame with the historic data (dates, prices, market capitalizations, and total volumes) for each coin ID
 def retrieving_data(ids):
@@ -118,22 +120,19 @@ def retrieving_data(ids):
     return historic_data
 
 # this is the main function that can be used to download the data
-def create_data_files(starting_index = 0, number_of_ids_per_data_subset = 100):
+def create_data_files(starting_index = 0, ids_per_data_subset = 100):
     # retrieving a list of all coin IDs
     # the initial API call returns a list of dictionaries with detailed information
     coin_list = cg.get_coins_list()
     list_length = len(coin_list)
     path = "/Users/Marc/Desktop/Past Affairs/Past Universities/SSE Courses/Master Thesis/Data"
-    # counter for the progress information
-    percentage_counter = 0
+
     while True:
         # printing the progress 
         progress = int(starting_index / list_length * 100)
-        if progress >= percentage_counter:
-            percentage_counter += 1
-            print("The overall progress is: " + str(progress) + "%")
+        print("The overall progress is: " + str(progress) + "%")
         
-        ids, ending_index = filtered_ids(coin_list, starting_index, number_of_ids_per_data_subset)
+        ids, ending_index = filtered_ids(coin_list, starting_index, ids_per_data_subset)
         # exporting the data in a data frame to a path specified by the user
         # the starting index is stored
         retrieving_data(ids).to_csv(path + "/cg_data.csv" + str(starting_index), index=False)
